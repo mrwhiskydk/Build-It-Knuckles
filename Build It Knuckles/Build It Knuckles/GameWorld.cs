73472f50a96a2708ed1038ebc874b0b030ac1d78
Jonathan Herning Gitz-Johansen <jona176r@student.eadania.dk>
2019-02-15 11:46:29 +0100

    dwq

--- Build It Knuckles/Build It Knuckles/GameWorld.cs
+++ Build It Knuckles/Build It Knuckles/GameWorld.cs
@@ -7,12 +7,12 @@ namespace Build_It_Knuckles
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class GameWorld : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        public Game1()
+        public GameWorld()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
---
+++ Build It Knuckles/Build It Knuckles/TownHall.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Build_It_Knuckles
+{
+    public class TownHall
+    {
+    }
+}
