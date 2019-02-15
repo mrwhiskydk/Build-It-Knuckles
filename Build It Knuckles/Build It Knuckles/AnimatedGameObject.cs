c5fead992cffc5ea9fe36694045e64edb74b5b77
Jonathan Herning Gitz-Johansen <jona176r@student.eadania.dk>
2019-02-15 12:03:22 +0100

    FEJL

--- Build It Knuckles/Build It Knuckles/AnimatedGameObject.cs
+++ Build It Knuckles/Build It Knuckles/AnimatedGameObject.cs
@@ -1,11 +1,71 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Build_It_Knuckles
 {
-    public class AnimatedGameObject
+    public class AnimatedGameObject : GameObject
     {
+        Rectangle[] animationRectangles;
+
+        float animationFPS = 10;
+        int currentAnimationIndex = 0;
+        double timeElapsed = 0;
+
+
+        public override Rectangle CollisionBox
+        {
+            get
+            {
+                return new Rectangle((int)(position.X - animationRectangles[0].Width * 0.5), (int)(position.Y - animationRectangles[0].Height * 0.5), animationRectangles[0].Width, animationRectangles[0].Height);
+            }
+        }
+
+        public AnimatedGameObject(int frameCount, float animationFPS, string spriteName) : this(frameCount, animationFPS, Vector2.Zero, spriteName)
+        {
+
+        }
+
+        public AnimatedGameObject(int frameCount, float animationFPS) : base(startPostion, spriteName)
+        {
+            this.animationFPS = animationFPS;
+            animationRectangles = new Rectangle[frameCount];
+            for (int i = 0; i < frameCount; i++)
+            {
+                animationRectangles[i] = new Rectangle(i * (sprite.Width / frameCount), 0, (sprite.Width / frameCount), sprite.Height);
+            }
+            currentAnimationIndex = 0;
+        }
+
+        /// <summary>
+        /// Updates the GameObject's logic and progresses the animation cycle
+        /// </summary>
+        /// <param name="gameTime">Takes a GameTime the provides the timespan since last call to update </param>
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            timeElapsed += gameTime.ElapsedGameTime.TotalSeconds;
+            currentAnimationIndex = (int)(timeElapsed * animationFPS);
+
+            if (currentAnimationIndex > animationRectangles.Count() - 1)
+            {
+                currentAnimationIndex = 0;
+                timeElapsed = 0;
+            }
+
+        }
+
+
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(sprite, position, animationRectangles[currentAnimationIndex], Color.White, rotation, new Vector2(animationRectangles[currentAnimationIndex].Width * 0.5f, animationRectangles[currentAnimationIndex].Height * 0.5f), 1f, new SpriteEffects(), 0f);
+        }
+
+
     }
 }
--- Build It Knuckles/Build It Knuckles/GameWorld.cs
+++ Build It Knuckles/Build It Knuckles/GameWorld.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using System.Collections.Generic;
 
 namespace Build_It_Knuckles
 {
@@ -9,13 +11,57 @@ namespace Build_It_Knuckles
     /// </summary>
     public class GameWorld : Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        private SpriteBatch spriteBatch;
+        private Texture2D background;
+        private Rectangle exampleRect;
+        private List<GameObject> gameObjects = new List<GameObject>();
+        private static List<GameObject> toBeAdded = new List<GameObject>();
+        private static List<GameObject> toBeRemoved = new List<GameObject>();
+
+        private SpriteFont font;
+        private Texture2D collisionTexture;
+
+        private static GraphicsDeviceManager graphics;
+
+        public static Rectangle ScreenSize
+        {
+            get
+            {
+                return graphics.GraphicsDevice.Viewport.Bounds;
+            }
+        }
+
+
+        private static ContentManager _content;
+        public static ContentManager ContentManager
+        {
+            get
+            {
+                return _content;
+            }
+        }
+
+
+
 
         public GameWorld()
         {
             graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 1920;  // set this value to the desired width of your window
+            graphics.PreferredBackBufferHeight = 1020;   // set this value to the desired height of your window
+            graphics.ApplyChanges();
             Content.RootDirectory = "Content";
+            // _content = Content;
+        }
+
+        public static void AddGameObject(GameObject go)
+        {
+            toBeAdded.Add(go);
+        }
+
+        public static void RemoveGameObject(GameObject go)
+        {
+            toBeRemoved.Add(go);
         }
 
         /// <summary>
@@ -37,11 +83,20 @@ namespace Build_It_Knuckles
         /// </summary>
         protected override void LoadContent()
         {
+
+
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            font = Content.Load<SpriteFont>("ExampleFont");
+            background = Content.Load<Texture2D>("stars");
+            exampleRect = new Rectangle(0, 0, 1920, 1020);
+            collisionTexture = Content.Load<Texture2D>("CollisionTexture");
 
-            // TODO: use this.Content to load your game content here
         }
+
+
+
+        //go = new AnimatedGameObject(4,20,Content,"HeroStrip");
 
         /// <summary>
         /// UnloadContent will be called once per game and is the place to unload
@@ -62,7 +117,29 @@ namespace Build_It_Knuckles
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            foreach (GameObject go in gameObjects)
+            {
+                go.Update(gameTime);
+                foreach (GameObject other in gameObjects)
+                {
+                    if (go != other && go.IsColliding(other))
+                    {
+                        go.DoCollision(other);
+                    }
+                }
+            }
+
+            foreach (GameObject go in toBeRemoved)
+            {
+                gameObjects.Remove(go);
+            }
+            toBeRemoved.Clear();
+
+            gameObjects.AddRange(toBeAdded);
+            toBeAdded.Clear();
+
+
+
 
             base.Update(gameTime);
         }
@@ -74,10 +151,32 @@ namespace Build_It_Knuckles
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+            spriteBatch.Draw(background, exampleRect, Color.White);
+            foreach (GameObject go in gameObjects)
+            {
+                go.Draw(spriteBatch);
+#if DEBUG
+                DrawCollisionBox(go);
+#endif
+            }
 
-            // TODO: Add your drawing code here
-
+            spriteBatch.End();
             base.Draw(gameTime);
+        }
+
+        private void DrawCollisionBox(GameObject go)
+        {
+            Rectangle collisionBox = go.CollisionBox;
+            Rectangle topLine = new Rectangle(collisionBox.X, collisionBox.Y, collisionBox.Width, 1);
+            Rectangle bottomLine = new Rectangle(collisionBox.X, collisionBox.Y + collisionBox.Height, collisionBox.Width, 1);
+            Rectangle rightLine = new Rectangle(collisionBox.X + collisionBox.Width, collisionBox.Y, 1, collisionBox.Height);
+            Rectangle leftLine = new Rectangle(collisionBox.X, collisionBox.Y, 1, collisionBox.Height);
+
+            spriteBatch.Draw(collisionTexture, topLine, null, Color.Red, 0, Vector2.Zero, SpriteEffects.None, 1);
+            spriteBatch.Draw(collisionTexture, bottomLine, null, Color.Red, 0, Vector2.Zero, SpriteEffects.None, 1);
+            spriteBatch.Draw(collisionTexture, rightLine, null, Color.Red, 0, Vector2.Zero, SpriteEffects.None, 1);
+            spriteBatch.Draw(collisionTexture, leftLine, null, Color.Red, 0, Vector2.Zero, SpriteEffects.None, 1);
         }
     }
 }
