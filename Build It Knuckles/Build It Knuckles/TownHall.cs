9b11745edf59a305f61a76d8b3ddccd616d870c6
Morten Bay Skov <mort79e5@student.eadania.dk>
2019-02-15 12:37:00 +0100

    77

--- Build It Knuckles/Build It Knuckles/TownHall.cs
+++ Build It Knuckles/Build It Knuckles/TownHall.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace Build_It_Knuckles
 {
     /// <summary>
     /// Public Class that represents the default functionality and game logic of the TownHall GameObject
     /// </summary>
-    public class TownHall
+    public class TownHall : GameObject
     {
+
+        /// <summary>
+        /// The TownHall Constructor, that sets the default starting position and sprite name of the current TownHall GameObject
+        /// </summary>
+        /// <param name="startPosition">The default position of where the TownHall is set in the game, on the X and Y Axis</param>
+        /// <param name="spriteName">The default name of the TownHall sprite</param>
+        public TownHall(Vector2 startPosition, string spriteName) : base(startPosition, spriteName)
+        {
+        }
     }
 }
